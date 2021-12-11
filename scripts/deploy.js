f82a7489b95b53f9e3096afdc92db5ccaeb35ee0
kevinqmcdonald <kevinqmcdonald@gmail.com>
2021-12-11 16:54:26 -0500

    Make more generic

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Kata = await hre.ethers.getContractFactory("Kata");
+  const kata = await Kata.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await kata.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Kata deployed to:", kata.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
